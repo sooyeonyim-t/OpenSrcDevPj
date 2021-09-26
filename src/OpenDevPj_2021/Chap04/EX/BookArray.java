acda5d449979c80684605543e1a0d10b94d6ecef
sooyeonyim <yimsooyeon9@gmail.com>
2021-09-26 19:39:02 +0900

    update Chap04 EX

--- src/OpenDevPj_2021/Chap04/EX/BookArray.java
+++ src/OpenDevPj_2021/Chap04/EX/BookArray.java
@@ -2,6 +2,14 @@ package OpenDevPj_2021.Chap04.EX;
 
 import java.util.Scanner;
 
+class Book3 {
+    String title,author;
+    public Book3(String title, String author){
+        this.title=title;
+        this.author=author;
+    }
+}
+
 public class BookArray {
     public static void main(String[] args) {
         Book3[] book = new Book3[2];
@@ -23,3 +31,4 @@ public class BookArray {
 
     }
 }
+
--- src/OpenDevPj_2021/Chap04/EX/CircleArray.java
+++ src/OpenDevPj_2021/Chap04/EX/CircleArray.java
@@ -1,16 +1,21 @@
 package OpenDevPj_2021.Chap04.EX;
 
+class Circle3 {
+    int radius;
+    public Circle3(int radius) {
+        this.radius = radius;
+    }
+    public double getArea() {
+        return 3.14*radius*radius;
+    }
+}
 public class CircleArray {
     public static void main(String[] args) {
-        Circle3[] c;
+        Circle3 [] c;
         c = new Circle3[5];
-
-        for (int i = 0; i < c.length; i++) {
+        for(int i=0; i<c.length; i++)
             c[i] = new Circle3(i);
-
-        }
-        for (int i = 0; i < c.length; i++) {
-            System.out.print((int) (c[i].getArea()) + " ");
-        }
+        for(int i=0; i<c.length; i++)
+            System.out.print((int)(c[i].getArea()) + " ");
     }
 }
